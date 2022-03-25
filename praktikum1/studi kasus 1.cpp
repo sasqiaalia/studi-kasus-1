3c89a3b45274c591cb28916de63a0deb58aa5888
sasqiaalia <auliasaskia93@gmail.com>
2022-03-25 21:01:07 +0700

    STUDI KASUS 1

---
+++ praktikum1/studi kasus 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+	float liter, hasil, total;
+	string nama;
+	int n;
+	
+	cout<<"===============================================\n";
+	cout << "STRUK SPBU UIUIUIUIUI\n";
+	cout << "Nama Pelanggan : ";
+	cin >> nama;
+	cout << "1.Pertalite \n2.Pertamax \nMasukan jenis BBM : ";
+	cin >> n;
+	cout << "Masukan pembelian perliter : ";
+	cin >> liter;
